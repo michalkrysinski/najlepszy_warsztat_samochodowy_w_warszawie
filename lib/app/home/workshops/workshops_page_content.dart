e8675ebd6cc677d5bdb4f85f7c2949c082d823ab
Michał Krysiński <mihalkrysinski8@gmail.com>
2022-01-23 12:45:36 +0100

    Install cloud firestore

---
+++ lib/app/home/workshops/workshops_page_content.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class WorkshopsPageContent extends StatelessWidget {
+  const WorkshopsPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance.collection('workshops').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text("Loading"));
+          }
+
+          final documents = snapshot.data!.docs;
+
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(document['name']),
+                          Text(document['warsztat']),
+                        ],
+                      ),
+                      Text(document['raiting'].toString()),
+                    ],
+                  ),
+                ),
+              ]
+            ],
+          );
+        });
+  }
+}
