4eba6b49dfc85fd4d2de923685354cd8c86bee70
Michał Krysiński <mihalkrysinski8@gmail.com>
2022-01-23 12:56:22 +0100

    Implement adding workshops

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var workshopsName = '';
+  var warsztatName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dwa'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration:
+                const InputDecoration(hintText: 'Podaj nazwę warsztatu'),
+            onChanged: (newValue) {
+              setState(() {
+                workshopsName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(hintText: 'Podaj nazwę ulicy'),
+            onChanged: (newValue) {
+              setState(() {
+                warsztatName = newValue;
+              });
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('workshops').add({
+                'name': workshopsName,
+                'warsztat': warsztatName,
+                'raiting': 5.5,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }
