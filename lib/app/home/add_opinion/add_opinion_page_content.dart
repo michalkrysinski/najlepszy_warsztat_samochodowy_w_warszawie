94ac6bbc46280606ee99317978a9b9aa68915711
Michał Krysiński <mihalkrysinski8@gmail.com>
2022-01-23 13:41:38 +0100

    Change bottom navigation bar index to 0 on save

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     Key? key,
+    required this.onSave,
   }) : super(key: key);
+
+  final Function onSave;
 
   @override
   State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
@@ -18,49 +21,55 @@ class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            decoration:
-                const InputDecoration(hintText: 'Podaj nazwę warsztatu'),
-            onChanged: (newValue) {
-              setState(() {
-                workshopsName = newValue;
-              });
-            },
-          ),
-          TextField(
-            decoration: const InputDecoration(hintText: 'Podaj nazwę ulicy'),
-            onChanged: (newValue) {
-              setState(() {
-                warsztatName = newValue;
-              });
-            },
-          ),
-          Slider(
-            onChanged: (newValue) {
-              setState(() {
-                rating = newValue;
-              });
-            },
-            value: rating,
-            min: 1.0,
-            max: 6.0,
-            divisions: 10,
-            label: rating.toString(),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseFirestore.instance.collection('workshops').add({
-                'name': workshopsName,
-                'warsztat': warsztatName,
-                'raiting': rating,
-              });
-            },
-            child: const Text('Dodaj'),
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration:
+                  const InputDecoration(hintText: 'Podaj nazwę warsztatu'),
+              onChanged: (newValue) {
+                setState(() {
+                  workshopsName = newValue;
+                });
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(hintText: 'Podaj nazwę ulicy'),
+              onChanged: (newValue) {
+                setState(() {
+                  warsztatName = newValue;
+                });
+              },
+            ),
+            Slider(
+              onChanged: (newValue) {
+                setState(() {
+                  rating = newValue;
+                });
+              },
+              value: rating,
+              min: 1.0,
+              max: 6.0,
+              divisions: 10,
+              label: rating.toString(),
+            ),
+            ElevatedButton(
+              onPressed: workshopsName.isEmpty || warsztatName.isEmpty
+                  ? null
+                  : () {
+                      FirebaseFirestore.instance.collection('workshops').add({
+                        'name': workshopsName,
+                        'warsztat': warsztatName,
+                        'raiting': rating,
+                      });
+                      widget.onSave();
+                    },
+              child: const Text('Dodaj'),
+            ),
+          ],
+        ),
       ),
     );
   }
