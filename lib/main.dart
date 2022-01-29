eabca21f5c3e957f448b68a704aa497b4a544f74
Michał Krysiński <mihalkrysinski8@gmail.com>
2022-01-29 13:54:38 +0100

    Move MyApp to app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:najlepszy_warsztat_samochodowy_w_warszawie/app/features/home/home_page.dart';
-import 'package:najlepszy_warsztat_samochodowy_w_warszawie/app/features/login/login_page.dart';
-import 'firebase_options.dart';
+import 'package:najlepszy_warsztat_samochodowy_w_warszawie/app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RootPage(),
-    );
-  }
-}
-
-class RootPage extends StatelessWidget {
-  const RootPage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        final user = snapshot.data;
-        if (user == null) {
-          return LoginPage();
-        }
-        return HomePage(user: user);
-      },
-    );
-  }
 }
