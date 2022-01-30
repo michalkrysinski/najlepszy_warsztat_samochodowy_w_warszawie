513a9932f2be957e964b8194a481f814f45f36a1
Michał Krysiński <mihalkrysinski8@gmail.com>
2022-01-30 08:16:23 +0100

    Add import in file main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:najlepszy_warsztat_samochodowy_w_warszawie/app/app.dart';
+import 'package:najlepszy_warsztat_samochodowy_w_warszawie/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
